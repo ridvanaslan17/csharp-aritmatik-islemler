60ac2807595bbec07b73ec3b9c851d8edc686ff0
ridvanaslan17 <ridvanaslan.work@gmail.com>
2023-12-10 23:51:57 +0300

    feature

---
+++ uygulama2/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace uygulama2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBoxTopla = new System.Windows.Forms.TextBox();
+            this.textBoxCikar = new System.Windows.Forms.TextBox();
+            this.textBoxCarp = new System.Windows.Forms.TextBox();
+            this.textBoxBol = new System.Windows.Forms.TextBox();
+            this.textBoxKalan = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBoxUs = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBoxOrt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(30, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Birinci sayı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(30, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "İkinci sayı";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(29, 214);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "TOPLAMA";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(156, 214);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "ÇIKARTMA";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(295, 214);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "ÇARPMA";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(424, 214);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "BÖLME";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(561, 214);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(55, 16);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "KALAN";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(178, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox2.Location = new System.Drawing.Point(178, 70);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBoxTopla
+            // 
+            this.textBoxTopla.Location = new System.Drawing.Point(32, 253);
+            this.textBoxTopla.Name = "textBoxTopla";
+            this.textBoxTopla.Size = new System.Drawing.Size(75, 20);
+            this.textBoxTopla.TabIndex = 9;
+            // 
+            // textBoxCikar
+            // 
+            this.textBoxCikar.Location = new System.Drawing.Point(159, 253);
+            this.textBoxCikar.Name = "textBoxCikar";
+            this.textBoxCikar.Size = new System.Drawing.Size(80, 20);
+            this.textBoxCikar.TabIndex = 10;
+            // 
+            // textBoxCarp
+            // 
+            this.textBoxCarp.Location = new System.Drawing.Point(298, 253);
+            this.textBoxCarp.Name = "textBoxCarp";
+            this.textBoxCarp.Size = new System.Drawing.Size(67, 20);
+            this.textBoxCarp.TabIndex = 11;
+            // 
+            // textBoxBol
+            // 
+            this.textBoxBol.Location = new System.Drawing.Point(427, 253);
+            this.textBoxBol.Name = "textBoxBol";
+            this.textBoxBol.Size = new System.Drawing.Size(68, 20);
+            this.textBoxBol.TabIndex = 12;
+            // 
+            // textBoxKalan
+            // 
+            this.textBoxKalan.Location = new System.Drawing.Point(564, 253);
+            this.textBoxKalan.Name = "textBoxKalan";
+            this.textBoxKalan.Size = new System.Drawing.Size(65, 20);
+            this.textBoxKalan.TabIndex = 13;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(178, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "HESAPLA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(677, 214);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(49, 16);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "ÜSSÜ";
+            // 
+            // textBoxUs
+            // 
+            this.textBoxUs.Location = new System.Drawing.Point(680, 253);
+            this.textBoxUs.Name = "textBoxUs";
+            this.textBoxUs.Size = new System.Drawing.Size(71, 20);
+            this.textBoxUs.TabIndex = 16;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(804, 217);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(66, 13);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "ORTALAMA";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // textBoxOrt
+            // 
+            this.textBoxOrt.Location = new System.Drawing.Point(798, 253);
+            this.textBoxOrt.Name = "textBoxOrt";
+            this.textBoxOrt.Size = new System.Drawing.Size(72, 20);
+            this.textBoxOrt.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(882, 289);
+            this.Controls.Add(this.textBoxOrt);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBoxUs);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxKalan);
+            this.Controls.Add(this.textBoxBol);
+            this.Controls.Add(this.textBoxCarp);
+            this.Controls.Add(this.textBoxCikar);
+            this.Controls.Add(this.textBoxTopla);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Aritmetik işlemler1 ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBoxTopla;
+        private System.Windows.Forms.TextBox textBoxCikar;
+        private System.Windows.Forms.TextBox textBoxCarp;
+        private System.Windows.Forms.TextBox textBoxBol;
+        private System.Windows.Forms.TextBox textBoxKalan;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBoxUs;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBoxOrt;
+    }
+}
+
---
+++ uygulama2/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uygulama2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double sayi1, sayi2, toplama, cikartma, carpma, bolme, kalan, us, ort;
+            
+            sayi1 = Convert.ToDouble(textBox1.Text);
+            sayi2 = Convert.ToDouble(textBox2.Text);
+
+
+
+            toplama = sayi1 + sayi2;
+            cikartma = sayi1 - sayi2;
+            carpma = sayi1 * sayi2;
+            bolme = sayi1 / sayi2;
+            kalan = sayi1 % sayi2;
+            us = Math.Pow(sayi1, sayi2);
+            ort = (sayi1 + sayi2) / 2;
+                
+                
+            textBoxTopla.Text = Convert.ToString(toplama);
+            textBoxCikar.Text = Convert.ToString(cikartma);
+            textBoxCarp.Text = Convert.ToString(carpma);
+            textBoxBol.Text = Convert.ToString(bolme);
+            textBoxKalan.Text = Convert.ToString(kalan);
+            textBoxUs.Text = Convert.ToString(us);
+            textBoxOrt.Text = Convert.ToString(ort);
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
